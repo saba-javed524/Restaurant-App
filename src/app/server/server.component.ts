fa2cf2b374ab72f71a4a12bf4af246586ab5fdb3
saba-javed524 <49893677+saba-javed524@users.noreply.github.com>
2021-06-11 01:19:02 +0500

    First Commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+})
+export class ServerComponent {
+  serverID: number = 10;
+  serverStatus: string = 'Online';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server created yet.';
+  theInput = '';
+  ngOnInit(): void {}
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  onServerCreation() {
+    this.serverCreationStatus =
+      'YES! Server was created. And, its name is ' + this.theInput;
+  }
+  onAddingInput(event: Event) {
+    this.theInput = (<HTMLInputElement>event.target).value;
+  }
+}
